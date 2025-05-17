df481a579cf7c686a60d17159c71432256856abf
Cleavest <cleavest@protonmail.com>
2025-05-17 12:08:48 +0300

    config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig = {
     experimental: {
-        allowedDevOrigins: [
-            'http://128.140.98.82',
-            'http://cleavest.space:3001',
-            'http://cleavest.space:3000',
-        ], // Ή το domain σου
+        serverActions: {
+            allowedOrigins: ['cleavest.space', 'localhost:3001'],
+        },
     },
 };
 
